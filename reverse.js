650d38e200ced59f04280cde0dae295cb89cb1dc
Siglo-R <130428190+Siglo-R@users.noreply.github.com>
2023-07-06 11:37:37 +0800

    Update reverse.js

---
+++ reverse.js
@@ -0,0 +1,11 @@
+function reverse(word) {
+    let newWord = '';
+    for (let i = word.length - 1; i >= 0; i--) {
+      newWord += word[i];
+    }
+    return newWord;
+  }
+  
+  const word = 'hello';
+  const reversedWord = reverse(word);
+  console.log(reversedWord);
